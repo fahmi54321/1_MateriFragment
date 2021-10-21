0e67ee81ab18d58de0370a1172d243dd5e53da46
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-10-21 17:49:12 +0700

    Initial commit

---
+++ app/src/main/java/com/android/a1_materifragment/CategoryFragment.kt
@@ -0,0 +1,46 @@
+package com.android.a1_materifragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class CategoryFragment : Fragment(),View.OnClickListener {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnDetailCategory: Button = view.findViewById(R.id.btn_detail_category)
+        btnDetailCategory.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        val detailCategoryFragment = DetailCategoryFragment()
+        val bundle = Bundle()
+
+        bundle.putString(DetailCategoryFragment.EXTRA_NAME,"Lifestyle")
+        val description = "Kategori ini akan berisi produk-produk lifestyle"
+
+        detailCategoryFragment.arguments = bundle
+        detailCategoryFragment.description = description
+
+        val mFragmentManager = parentFragmentManager
+        mFragmentManager?.beginTransaction()?.apply {
+            replace(R.id.frame_container, detailCategoryFragment, DetailCategoryFragment::class.java.simpleName)
+            addToBackStack(null)
+            commit()
+
+        }
+
+    }
+}
